e421951c414b6c2873e4c34af9135c8986bab781
mvk-vinodhkumar <mvk.vinodhkumar@gmail.com>
2022-07-26 18:05:41 +0530

    Updates

---
+++ precache-manifest.b5e1f24bd6a6b8759b40eda62daa823e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f70c891f3879881c4214",
+    "url": "/tesla-clone/static/css/main.30ad165d.chunk.css"
+  },
+  {
+    "revision": "f70c891f3879881c4214",
+    "url": "/tesla-clone/static/js/main.ba7820b2.chunk.js"
+  },
+  {
+    "revision": "f367e2c30829abb4a50c",
+    "url": "/tesla-clone/static/js/runtime~main.4472d384.js"
+  },
+  {
+    "revision": "5b7cb58f424fa13de4ae",
+    "url": "/tesla-clone/static/js/2.e7c5f3b9.chunk.js"
+  },
+  {
+    "revision": "972912021bdf7e088519d5927a2e0ba1",
+    "url": "/tesla-clone/index.html"
+  }
+];
